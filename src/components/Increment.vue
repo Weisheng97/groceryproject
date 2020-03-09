9fbe8bc7374a62d7750d0bcaeb72f76c7bf2fbe7
Weisheng97 <c.weisheng97@gmail.com>
2020-03-09 15:50:44 +0800

    Week 7
    Original with router

---
+++ src/components/Increment.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class = "headerStyle">
+        <p1> Current Count: {{counter}} </p1>
+        <br> <br>
+        <button v-on:click="increment">+</button>
+        <button v-on:click="decrement">-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        increment: function() {
+            this.counter += 1;
+            
+        },
+        decrement: function() {
+            if(this.counter != 0) {
+                this.counter -= 1;  
+            }          
+        }
+    }
+}
+</script>
+
+<style scoped>
+header{
+    background:rgb(122, 63, 100);
+    padding:10px;
+    border-style: solid;
+    border-color:black;
+    border-width: 2px;
+}
+h1{
+    color:ivory
+}
+</style>
